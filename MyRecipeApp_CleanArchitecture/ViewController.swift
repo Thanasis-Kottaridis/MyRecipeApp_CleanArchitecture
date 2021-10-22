8de9c67d62cdb9455f74133d9d24a46e29c7d27b
thanasis kottaridis <thanasiskottaridis@yahoo.gr>
2021-10-23 02:06:58 +0300

    first commit

---
+++ MyRecipeApp_CleanArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyRecipeApp_CleanArchitecture
+//
+//  Created by thanos kottaridis on 23/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
